29d75f5efa205cbc499827dd9a0646c38e57a9fc
ElviraBudna <elvira.budna@gmail.com>
2023-03-14 22:41:22 +0200

    b

--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
 import './css/styles.css';
-import fetchCountries from './js/fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
-const ref = {
+const refs = {
   input: document.querySelector('#search-box'),
+  countriesList: document.querySelector('.country-list'),
+  countryInfoCard: document.querySelector('.country-info'),
 };
 const onInputDebounced = debounce(onInput, DEBOUNCE_DELAY);
 
-ref.input.addEventListener('input', onInputDebounced);
+refs.input.addEventListener('input', onInputDebounced);
 
 function onInput(e) {
   const nameCountry = e.target.value.trim();
+  //   if (nameCountry === '') {
+  //     return;
+  //   }
   fetchCountries(nameCountry)
-    .then(country => console.log(country))
+    .then(country => {
+      if (country.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (country.length >= 2 && country.length <= 10) {
+        refs.countriesList.insertAdjacentHTML(
+          'beforeend',
+          createCountriesList(country)
+        );
+        console.log('7');
+      } else if (country.length === 1) {
+        console.log('1');
+      }
+      console.log(country);
+    })
     .catch(error => {
       console.log(error);
+      //   Notiflix.Notify.failure('Oops, there is no country with that name');
     });
+}
+
+function createCountriesList(countries) {
+  return countries
+    .map(({ flags, name }) => {
+      return `<li class='country-element'><img src="${flags.svg}" alt="${flags.alt}" width=30px height=30px><h2>${name.common}<h2></li>`;
+    })
+    .join('');
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
 
-export default function fetchCountries(name) {
+export function fetchCountries(name) {
   return fetch(
     `${BASE_URL}/name/${name}?fields=name.official,capital,population,flags.svg,languages`
   ).then(response => {
